16d6623b66e0ca3facfc9f7d3629db5febfd00c1
AleksandrPodkopaev <apodkopa@andrew.cmu.edu>
2019-03-22 18:37:52 -0400

    changed max to sum when defining eta and changed sparsify_neuron_corenet function to deal with sum of sensitivities being equal to 0

--- corenet.py
+++ corenet.py
@@ -35,6 +35,10 @@ def sparsify_neuron_corenet(mask, row, eps, delta, inp,
         inp {torch.FloatTensor} -- input to current layer ell,
                                    evaluated on set S, hence it
                                    is a vector of size (len(S),)
+
+        #   above inp is a matrix?
+
+
         eta, eta_star -- from enclosing scope
     
     Returns:
@@ -48,21 +52,36 @@ def sparsify_neuron_corenet(mask, row, eps, delta, inp,
     # For every active input connection, compute its max (normalized) activation
     assert 0 < torch.sum(mask), mask
     sensit = (inp * row * mask).detach().numpy()  # (bs, n_in), (n_in) - broadcast ok
+    # assert np.all(np.abs(sensit.sum(axis=1)) > 0), sensit.sum(axis=1)  # denom
+    
+    extra_sensit = sensit.T/sensit.sum(axis=1)
+    
+    # two lines below assure that there are no Nan values / no need for assert
+    where_are_NaNs = np.isnan(extra_sensit)
+    extra_sensit[where_are_NaNs] = 0
 
-    assert np.all(np.abs(sensit.sum(axis=1)) > 0), sensit.sum(axis=1)  # denom
-    sensit = ( sensit.T/sensit.sum(axis=1) ).max(axis=1)  # [0] - for pytorch
+    sensit = extra_sensit.max(axis=1)  # [0] - for pytorch
+
+
+    #sensit[null_indeces] = 0
+    #print(sensit)
     sum_sensit = sensit.sum()
     q = sensit / sum_sensit
+
 
     # Issue: this m is too huge
     # print(sum_sensit, np.log(eta*eta_star),np.log(8*eta/delta), eps**(-2))
     m = int(np.ceil(8 * sum_sensit * np.log(eta*eta_star)*np.log(8*eta/delta) / eps**2))
-    # print(m)
+
     m = 100
     # sample a multiset of neurons with probs q
     # TODO: len(row)
     assert np.all(q >= 0.), inp  # First-to-second layer sparsification fails
-    w_inds = np.random.choice(np.arange(len(row)), size=m, p=q)
+    w_inds = np.random.choice(np.arange(len(row)), size=m, p=q, replace=True) 
+    
+    ##replace=false added
+    
+    print(np.sum(q[w_inds]))
     w_new = torch.zeros_like(row)
     # for each ind of neuron, update corresponding w_new_j
     for ind in w_inds:
@@ -84,15 +103,17 @@ def sparsify_corenet(model, train, eps=0.5, delta=0.5):
     # compute parameters
     n_hidden = len(model.sizes_list)  # = L-1
     eps_prime = 0.5 * eps / n_hidden
-    eta = max(model.sizes_list)
+    eta = sum(model.sizes_list)     
+    
+    ### changed max to sum
     eta_star = max(model.sizes_list[:-1])
+
     lambda_star = np.log(eta * eta_star) / 2
     sq_ = np.sqrt(2 * lambda_star)
     kappa = sq_ * (1 + sq_ * np.log(8*eta*eta_star/delta))
 
     # sample the set S from Dataset train
     subset_size = int(np.ceil(np.log(8*eta*eta_star/delta)*np.log(eta*eta_star)))
-    print("Subset S of size {}".format(subset_size))
     S = sample_from_dataset(train, subset_size).to(DEVICE)
 
     input_activations = S  # (batch_size, num_input_features)
@@ -103,6 +124,7 @@ def sparsify_corenet(model, train, eps=0.5, delta=0.5):
         print("First layer:", m)
         if isinstance(m, nn.Linear):
             # shape (batch_size, n_out)
+
             triangle_num = input_activations.abs().matmul(m.weight.transpose(0,1).abs())
             triangle_denom = input_activations.matmul(m.weight.transpose(0,1)).abs()
 
@@ -130,15 +152,15 @@ def sparsify_corenet(model, train, eps=0.5, delta=0.5):
         if isinstance(m, nn.Linear):
             # shape (batch_size, n_out)
             triangle_forward = np.prod(triangles[ell:])
-            eps_ell = eps_prime / triangle_forward
-
+            eps_ell = eps_prime / triangle_forward 
             n_out = m.out_features
             # for every neuron do (TODO: vectorize this loop,
             # most likely it will take nothing more then removing for)
             for i in range(n_out):
                 incoming_conn = m.weight[i, :]  # all connections to i-th neuron
-                pos_mask, neg_mask = incoming_conn.ge(0).float(), incoming_conn.le(0).float()
+                pos_mask, neg_mask = incoming_conn.gt(0).float(), (1-incoming_conn.gt(0)).float()
                 # sparsify both
+
                 W_pos = sparsify_neuron_corenet(pos_mask, incoming_conn,
                         eps_ell, delta, input_activations, eta, eta_star)
                 W_neg = sparsify_neuron_corenet(neg_mask, -incoming_conn,
